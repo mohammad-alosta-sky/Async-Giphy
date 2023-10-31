b5cf76898ff9a273a94e308edf3cc3212dd005f8
Mohammad Al osta <mohammad.alosta@sky.uk>
2023-10-31 16:39:56 +0000

    working

--- index.js
+++ index.js
@@ -7,19 +7,26 @@ async function getImage(query) {
     const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
     const response = await fetch(endpoint);
     const data = await response.json();
-    const img = data.data[Math.floor(Math.random()*24)].images.original.url;
+    const img = data.data[Math.floor(Math.random()*25)].images.original.url;
     return img
 }
 
 const getImageHelper = async () => {
-    console.log(await getImage("dog"));
+    console.log(await getImage("lol"));
 }
 
 
+// const alink = await getImage("lol");
+// console.log(alink)
+
+
 getImageHelper();
+
+
 // const img = getImage("lol");
 // console.log(img)
 
-// const button = document.querySelector("button");
-// button.addEventListener("click", (event) => {
-//     document.querySelector(".img").innerHTML = `<img src=${getImage("lol")}>`});
+const button = document.querySelector("button");
+button.addEventListener("click", async (event) => { 
+    const alink = await getImage("lol");
+    document.querySelector(".img").innerHTML = `<img src=${alink}>`});
